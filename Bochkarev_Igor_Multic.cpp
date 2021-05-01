76ca9de71ed2c175d7156c4f19ec22b91ca64feb
Biven21 <igbochkarev2017@gmail.com>
2021-05-01 17:46:19 +0500

    убрал функции в библиотеку.
    убрал ф-ии в библиотеку, добавил вызов библиотеки и имя пространства

--- Bochkarev_Igor_Multic.cpp
+++ Bochkarev_Igor_Multic.cpp
@@ -10,14 +10,12 @@
 //-----------------------------------------------------------------------------
 
 #include "TXLib.h"
+#include "BivenLib.h"
 
-void HouseDraw   (int x, int y, double sizeX, double sizeY, double GableUp, double GableX, double roofRight, double roofLeft);
-void SunDraw     (int x, int y, double size,  double sizeChet, double sizeNChet, double sizeEyse,
-                 double EyeLeftX, double EyeRightX, double EyeLeftUp, double EyeRightUp);
+using namespace Biven:
+
 void girlDraw    (int x, int y, double sizeX, double sizeY, double wind);
-void BoyDraw     (int x, int y, double sizeX, double sizeY, int shovelUp, int stepX, int stepY);
 void FirDraw     (int x, int y, double sizeX, double sizeY, double Wind);
-void TreeDraw    (int x, int y); //, double sizeX, double sizeY
 void BackGround  ();
 void StartTitles ();
 void StarShip    (int x, int y, double sizeShip);
@@ -58,65 +56,7 @@ int main()
 
 //-----------------------------------------------------------------------------
 
-void SunDraw (int x, int y, double size, double sizeChet, double sizeNChet, double sizeEyse,
-              double EyeLeftX, double EyeRightX, double EyeLeftUp, double EyeRightUp )
-    {
-    txSetColor     (TX_YELLOW, 4);
-    txSetFillColor (TX_YELLOW);
-    txCircle (x, y, 20*size);
-    txLine (ROUND (x + 15*size), ROUND (y - 14*size), ROUND (x + 40*size*sizeNChet), ROUND (y - 37*size*sizeNChet));
-    txLine (ROUND (x + 19*size), ROUND (y -  4*size), ROUND (x + 39*size*sizeChet),  ROUND (y -  7*size*sizeChet));
-    txLine (ROUND (x + 20*size), ROUND (y +  9*size), ROUND (x + 37*size*sizeNChet), ROUND (y + 18*size*sizeNChet));
-    txLine (ROUND (x +  8*size), ROUND (y + 18*size), ROUND (x + 16*size*sizeChet),  ROUND (y + 36*size*sizeChet));
-    txLine (ROUND (x),           ROUND (y + 20*size), ROUND (x),                     ROUND (y + 63*size*sizeNChet));
-    txLine (ROUND (x),           ROUND (y - 20*size), ROUND (x),                     ROUND (y - 63*size*sizeChet));
-    txLine (ROUND (x -  8*size), ROUND (y + 18*size), ROUND (x - 16*size*sizeNChet), ROUND (y + 36*size*sizeNChet));
-    txLine (ROUND (x - 15*size), ROUND (y - 14*size), ROUND (x - 40*size*sizeChet),  ROUND (y - 37*size*sizeChet));
-    txLine (ROUND (x - 20*size), ROUND (y +  9*size), ROUND (x - 37*size*sizeNChet), ROUND (y + 18*size*sizeNChet));
-    txLine (ROUND (x - 19*size), ROUND (y -  4*size), ROUND (x - 39*size*sizeChet),  ROUND (y -  7*size*sizeChet));
 
-    txSetColor (TX_BLACK, 1);
-    txSetFillColor (TX_WHITE);
-    txCircle (ROUND (x - 10*size*EyeLeftX),  ROUND (y - 10*size*EyeLeftUp),  4*size*sizeEyse);
-    txCircle (ROUND (x + 10*size*EyeRightX), ROUND (y - 10*size*EyeRightUp), 4*size*sizeEyse);
-    txSetFillColor (TX_BLACK);
-    txCircle (ROUND (x -  9*size*EyeLeftX),  ROUND (y - 10*size*EyeLeftUp),  2.7*size*sizeEyse);
-    txCircle (ROUND (x + 11*size*EyeRightX), ROUND (y - 10*size*EyeRightUp), 2.7*size*sizeEyse);
-
-    txSetFillColor (TX_TRANSPARENT);
-    POINT smile [5] = {{ROUND (x - 11*size), ROUND (y + 7*size)}, {ROUND (x),          ROUND (y + 13*size)},
-                       {ROUND (x + 11*size), ROUND (y + 7*size)}, {ROUND (x + 5*size), ROUND (y +  9*size)},
-                       {ROUND (x -  5*size), ROUND (y + 9*size)}};
-    txPolygon (smile, 5);
-    }
-
-//-----------------------------------------------------------------------------
-
-void HouseDraw (int x, int y, double sizeX, double sizeY, double GableUp, double GableX, double roofRight, double roofLeft)
-    {
-    txSetColor      (TX_GREEN);
-
-    txSetFillColor  (TX_GREEN);
-    txRectangle (x - 100*sizeX, y - 70*sizeY, x, y);
-
-    txSetFillColor  (TX_LIGHTGRAY);
-    txRectangle (x - 36*sizeX, y - 42*sizeY, x - 15*sizeX, y);
-
-    txSetFillColor  (TX_YELLOW);
-    txRectangle (x - 79*sizeX, y - 42*sizeY, x - 65*sizeX, y - 22*sizeY);
-    txLine      (x - 73*sizeX, y - 42*sizeY, x - 73*sizeX, y - 22*sizeY);
-
-    txSetFillColor  (TX_RED);
-    POINT roof [3] = {{ROUND (x - 120*sizeX*roofLeft),  ROUND (y -  70*sizeY)},
-                      {ROUND (x -  50*sizeX*GableX),    ROUND (y - 128*sizeY*GableUp)},
-                      {ROUND (x +  20*sizeX*roofRight), ROUND (y -  70*sizeY)}};
-    txPolygon  (roof, 3);
-
-    txSetFillColor  (TX_BROWN);
-    POINT chimney [4] = {{ROUND (x - 85*sizeX), ROUND (y - 99*sizeY)},          {ROUND (x - 85*sizeX), ROUND (y - 117*sizeY*GableUp)},
-                         {ROUND (x - 76*sizeX), ROUND (y - 117*sizeY*GableUp)}, {ROUND (x - 76*sizeX), ROUND (y - 107*sizeY)}};
-    txPolygon (chimney, 4);
-    }
 
 //-----------------------------------------------------------------------------
 
@@ -142,28 +82,6 @@ void girlDraw (int x, int y, double sizeX, double sizeY, double wind)
 
  //-----------------------------------------------------------------------------
 
-void BoyDraw (int x, int y, double sizeX, double sizeY, int shovelUp, int stepX, int stepY)
-    {
-    txSetColor  (TX_GREEN);
-    txCircle    (x, y - 5*sizeY, 5*((sizeX + sizeY) / 2));
-
-    txSetColor      (TX_BLACK, 2);
-    txSetFillColor  (TX_RED);
-    POINT shirt [4] = {{ROUND (x - 4*sizeX), ROUND (y)},            {ROUND (x + 6*sizeX), ROUND (y)},
-                       {ROUND (x + 3*sizeX), ROUND (y + 16*sizeY)}, {ROUND (x - 1*sizeX), ROUND (y +16*sizeY)}};
-    txPolygon (shirt, 4);
-
-    txLine (ROUND (x -  1*sizeX), ROUND (y + 16*sizeY),          ROUND (x -  (stepX / 2)*sizeX), ROUND (y + (27 - (stepY / 2)) * sizeY));
-    txLine (ROUND (x +  1*sizeX), ROUND (y + 16*sizeY),          ROUND (x +  (stepX / 2)*sizeX), ROUND (y + (27 + (stepY / 2)) * sizeY));
-    txLine (ROUND (x -  3*sizeX), ROUND (y +  3*sizeY),          ROUND (x - 14*sizeX),           ROUND (y +  6*sizeY*shovelUp));
-    txLine (ROUND (x +  5*sizeX), ROUND (y +  3*sizeY),          ROUND (x + 12*sizeX),           ROUND (y + 15*sizeY));
-    txLine (ROUND (x - 14*sizeX), ROUND (y +  3*sizeY*shovelUp), ROUND (x - 14*sizeX),           ROUND (y + 23*sizeY*shovelUp));
-
-    txSetFillColor  (TX_GRAY);
-    POINT shovel [4] = {{ROUND (x - 17*sizeX), ROUND (y + 23*sizeY*shovelUp)}, {ROUND (x - 11*sizeX), ROUND (y + 23*sizeY*shovelUp)},
-                        {ROUND (x - 12*sizeX), ROUND (y + 27*sizeY*shovelUp)}, {ROUND (x - 16*sizeX), ROUND (y + 27*sizeY*shovelUp)}};
-    txPolygon (shovel, 4);
-    }
 
  //-----------------------------------------------------------------------------
 
@@ -193,33 +111,6 @@ void FirDraw (int x, int y, double sizeX, double sizeY, double Wind)
     txPolygon  (Fir3Line, 3);
     }
 
-//-----------------------------------------------------------------------------
-
-void TreeDraw (int x, int y, double sizeX, double sizeY)
-    {
-    txSetColor (TX_BROWN, 4);
-    txLine   (x, y, x, y - 30 * sizeY);
-
-    txSetColor  (TX_BROWN, 2);
-    txLine   (x,           y -  8*sizeY, x +  9*sizeX, y - 20*sizeY);
-    txLine   (x +  9*sizeX, y - 20*sizeY, x + 18*sizeX, y - 23*sizeY);
-    txLine   (x,            y - 13*sizeY, x -  9*sizeX, y - 20*sizeY);
-    txLine   (x -  9*sizeX, y - 20*sizeY, x - 16*sizeX, y - 20*sizeY);
-    txLine   (x,            y - 20*sizeY, x +  9*sizeX, y - 37*sizeY);
-    txLine   (x,            y - 30*sizeY, x -  4*sizeX, y - 39*sizeY);
-
-    txSetColor  (TX_GREEN, 2);
-    txSetFillColor (TX_GREEN);
-    txCircle (x,            y - 30*sizeY, 3 * (sizeX + sizeY));
-    txCircle (x +  9*sizeX, y - 20*sizeY, 3 * (sizeX + sizeY));
-    txCircle (x + 18*sizeX, y - 23*sizeY, 3 * (sizeX + sizeY));
-    txCircle (x -  9*sizeX, y - 20*sizeY, 3 * (sizeX + sizeY));
-    txCircle (x - 16*sizeX, y - 20*sizeY, 3 * (sizeX + sizeY));
-    txCircle (x +  9*sizeX, y - 37*sizeY, 3 * (sizeX + sizeY));
-    txCircle (x -  4*sizeX, y - 39*sizeY, 3 * (sizeX + sizeY));
-    }
-
-//-----------------------------------------------------------------------------
 //_____________________________________________________________________________
 void BackGround ()
     {
