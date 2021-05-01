aad2f3d5cffb12bc39315ce3c1720f31276d665f
Biven21 <82120731+Biven21@users.noreply.github.com>
2021-05-01 10:59:25 +0400

    file created, still empty.
    создан файл. Только шапка.

---
+++ BivenLib.h
@@ -0,0 +1,17 @@
+//==================================================================
+//@file BivenLib.h                                                //
+//                                                                //
+//  librari of functions for a novice programmer Biven            //
+//  библиотека начинающего программиста Бочкарев Игорь Викторович //
+//                                                                //
+// (c) Bochkarev Igor V, teachar at school number 175 in Samara.  //
+//                                                                //
+//@date                  01/05/2021                               //
+//==================================================================
+
+#ifndef BIVENLIB_H_INCLUDED
+#define BIVENLIB_H_INCLUDED
+
+
+
+#endif // BIVENLIB_H_INCLUDED
