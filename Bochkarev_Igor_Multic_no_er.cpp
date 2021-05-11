182def20a300872dcc56218d64d93c3731591e12
Biven21 <82120731+Biven21@users.noreply.github.com>
2021-05-11 22:46:00 +0400

    Перенес ф-ю  HouseDraw в библиотеку
    Все работает

--- Bochkarev_Igor_Multic_no_er.cpp
+++ Bochkarev_Igor_Multic_no_er.cpp
@@ -10,8 +10,9 @@
 //-----------------------------------------------------------------------------
 
 #include "TXLib.h"
+#include "Biven_Er.h"
 
-void HouseDraw   (int x, int y, double sizeX, double sizeY, double GableUp, double GableX, double roofRight, double roofLeft);
+
 void SunDraw     (int x, int y, double size,  double sizeChet, double sizeNChet, double sizeEyse,
                  double EyeLeftX, double EyeRightX, double EyeLeftUp, double EyeRightUp);
 void girlDraw    (int x, int y, double sizeX, double sizeY, double wind);
@@ -92,33 +93,6 @@ void SunDraw (int x, int y, double size, double sizeChet, double sizeNChet, doub
     txPolygon (smile, 5);
     }
 
-//-----------------------------------------------------------------------------
-
-void HouseDraw (int x, int y, double sizeX, double sizeY, double GableUp, double GableX, double roofRight, double roofLeft)
-    {
-    txSetColor      (TX_GREEN);
-
-    txSetFillColor  (TX_GREEN);
-    txRectangle (x - 100*sizeX, y - 70*sizeY, x, y);
-
-    txSetFillColor  (TX_LIGHTGRAY);
-    txRectangle (x - 36*sizeX, y - 42*sizeY, x - 15*sizeX, y);
-
-    txSetFillColor  (TX_YELLOW);
-    txRectangle (x - 79*sizeX, y - 42*sizeY, x - 65*sizeX, y - 22*sizeY);
-    txLine      (x - 73*sizeX, y - 42*sizeY, x - 73*sizeX, y - 22*sizeY);
-
-    txSetFillColor  (TX_RED);
-    POINT roof [3] = {{ROUND (x - 120*sizeX*roofLeft),  ROUND (y -  70*sizeY)},
-                      {ROUND (x -  50*sizeX*GableX),    ROUND (y - 128*sizeY*GableUp)},
-                      {ROUND (x +  20*sizeX*roofRight), ROUND (y -  70*sizeY)}};
-    txPolygon  (roof, 3);
-
-    txSetFillColor  (TX_BROWN);
-    POINT chimney [4] = {{ROUND (x - 85*sizeX), ROUND (y - 99*sizeY)},          {ROUND (x - 85*sizeX), ROUND (y - 117*sizeY*GableUp)},
-                         {ROUND (x - 76*sizeX), ROUND (y - 117*sizeY*GableUp)}, {ROUND (x - 76*sizeX), ROUND (y - 107*sizeY)}};
-    txPolygon (chimney, 4);
-    }
 
 //-----------------------------------------------------------------------------
 
